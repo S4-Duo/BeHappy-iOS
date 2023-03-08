0f9f1a6f57afc2cbc56dd3d5c0e1c8b4da3c17db
Brett Mulder <brettmulder@Bretts-MacBook-Pro.local>
2023-03-08 09:20:23 +0100

    Added settings view

---
+++ BeHappy-iOS/BeHappy-iOS/Views/SettingsView.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsView.swift
+//  BeHappy-iOS
+//
+//  Created by Brett Mulder on 08/03/2023.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        NavigationStack{
+            Text("Hello world")
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
