90934e453bd3154e88ef38ff7339d2472f7c6a14
Reno Muijsenberg <r.muijsenberg@student.fontys.nl>
2023-03-14 09:19:43 +0100

    Add everything to a service

--- BeHappy-iOS/BeHappy-iOS/Views/CommunityView.swift
+++ BeHappy-iOS/BeHappy-iOS/Views/CommunityView.swift
@@ -10,11 +10,10 @@ import Firebase
 import FirebaseFirestore
 
 struct CommunityView: View {
-    let db = Firestore.firestore()
+    let friendsService = FireStoreFriendsService()
     
     @State var users = [UserModel]()
     @State var friendsList = [FriendModel]()
-    
     
     var body: some View {
         VStack {
@@ -28,15 +27,19 @@ struct CommunityView: View {
 
                             if friendsList.first(where: { $0.id == user.id }) != nil {
                                 Button(action: {
-                                     removeFriend(friendId: user.id)
+                                    friendsService.removeFriend(friendId: user.id, friendsList: friendsList) { friends in
+                                        friendsList = friends
+                                    }
                                 }, label: {
                                     Text("Remove")
                                         .padding(10)
+                                        .foregroundColor(.red)
                                 })
-                                .buttonStyle(.borderedProminent)
                             } else {
                                 Button(action: {
-                                     addFriend(friendId: user.id)
+                                    friendsService.addFriend(friendId: user.id, friendsList: friendsList) { friends in
+                                        friendsList = friends
+                                    }
                                 }, label: {
                                     Text("Follow")
                                         .padding(10)
@@ -49,78 +52,12 @@ struct CommunityView: View {
             }
         }
         .onAppear {
-            getAllUsers()
-            getAllFriends()
-        }
-    }
-    
-    private func getAllUsers() {
-        users = [] ///Set array empty so it does not duplicate
-        
-        db.collection("users").getDocuments { (querySnapshot, error) in
-            if let error = error {
-                print("Error getting documents: \(error)")
-            } else {
-                for document in querySnapshot!.documents {
-                    let userId = document.documentID
-                    let data = document.data()
-                    
-                    let user = UserModel(userId: userId, userName: data["userName"] as? String ?? "")
-                    
-                    users.append(user)
-                }
+            friendsService.getAllUsers() { usersList in
+                users = usersList
             }
-        }
-    }
-    
-    private func getAllFriends() {
-        friendsList = [] ///Set array empty so it does not duplicate
-        
-        if let currentUser = Auth.auth().currentUser {
-            db.collection("friends").document(currentUser.uid).getDocument { (document, error) in
-                if let error = error {
-                    print("Error getting document: \(error)")
-                } else if let document = document, document.exists {
-                    if let friendsArray = document.data()?["friends"] as? [String] {
-                        let friendPaths = friendsArray.map { $0 }
-                        
-                        for id in friendPaths {
-                            friendsList.append(FriendModel(friendId: id))
-                        }
-                    } else {
-                        print("friendsRef field is missing or not an array")
-                    }
-                } else {
-                    print("Document does not exist")
-                }
+            friendsService.getAllFriends() { friends in
+                friendsList = friends
             }
-        }
-    }
-    
-    private func addFriend(friendId: String) {
-        var friendIdList = [String]()
-        friendsList.append(FriendModel(friendId: friendId))
-        
-        for friendId in friendsList {
-            friendIdList.append(friendId.id)
-        }
-        
-        if let currentUser = Auth.auth().currentUser {
-            db.collection("friends").document(currentUser.uid).setData(["friends": friendIdList])
-        }
-    }
-    
-    private func removeFriend(friendId: String) {
-        var friendIdList = [String]()
-        
-        friendsList.removeAll { $0.id == friendId }
-        
-        for friendId in friendsList {
-            friendIdList.append(friendId.id)
-        }
-        
-        if let currentUser = Auth.auth().currentUser {
-            db.collection("friends").document(currentUser.uid).setData(["friends": friendIdList])
         }
     }
 }
