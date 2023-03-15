be935f0892ccd280dd49810ef3635ef7398fdfbe
Brett Mulder <brettmulder@Bretts-MacBook-Pro.local>
2023-03-15 09:45:34 +0100

    Added animated scroll

--- BeHappy-iOS/BeHappy-iOS/Views/PagerView.swift
+++ BeHappy-iOS/BeHappy-iOS/Views/PagerView.swift
@@ -11,7 +11,8 @@ struct PagerView<Content: View>: View {
     let pageCount: Int
     @Binding var currentIndex: Int
     let content: Content
-    
+    @State private var isScrolling = false // Add this state variable
+
     init(pageCount: Int, currentIndex: Binding<Int>, @ViewBuilder content: () -> Content) {
         self.pageCount = pageCount
         self._currentIndex = currentIndex
@@ -29,14 +30,20 @@ struct PagerView<Content: View>: View {
             .content.offset(y: CGFloat(currentIndex) * -geometry.size.height)
             .frame(width: geometry.size.width, height: geometry.size.height, alignment: .top)
             .gesture(DragGesture()
-                        .onEnded({ value in
-                            if value.translation.height < -50 {
-                                currentIndex = min(currentIndex + 1, pageCount - 1)
-                            }
-                            if value.translation.height > 50 {
-                                currentIndex = max(currentIndex - 1, 0)
-                            }
-                        }))
+                .onChanged { _ in
+                    isScrolling = true // Set isScrolling to true when the user starts dragging
+                }
+                .onEnded({ value in
+                    if value.translation.height < -50 {
+                        currentIndex = min(currentIndex + 1, pageCount - 1)
+                    }
+                    if value.translation.height > 50 {
+                        currentIndex = max(currentIndex - 1, 0)
+                    }
+                    isScrolling = false // Set isScrolling to false when the user stops dragging
+                })
+            )
+            .animation(isScrolling ? .easeInOut(duration: 0.2) : nil) // Apply animation only when isScrolling is true
         }
     }
 }
